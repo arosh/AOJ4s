0ae64b40b840d53e6658cadfa762fbba556af39e
Sho Iizuka <sho.i518@gmail.com>
2012-02-24 22:53:11 +0900

    add implicit SleepTime

--- src/main/scala/com/github/arosh/aoj4s/AOJ.scala
+++ src/main/scala/com/github/arosh/aoj4s/AOJ.scala
@@ -5,19 +5,25 @@ import query._
 import info._
 
 object AOJ extends QueryWrapper {
-  var basedURL = "http://judge.u-aizu.ac.jp/onlinejudge/webservice/"
-  var SLEEP_TIME = 10000
+  private val AOJ_URI = "http://judge.u-aizu.ac.jp/onlinejudge/webservice/"
+
+  private type Tagged[U] = { type Tag = U }
+  protected class SleepTimeTag
+  type SleepTime = Int with Tagged[SleepTimeTag]
+
+  implicit def Int2SleepTime(i: Int): SleepTime = i.asInstanceOf[SleepTime]
+  private val DEFAULT_SLEEP_TIME = 10000
 
   private def joinQuery[T <: Query[_]](queries: T*): String = {
     queries.withFilter(null !=).map(p => "%s=%s".format(p.field, p.value)).mkString("?", "&", "")
   }
 
-  private def loadXML(params: String): Option[xml.Elem] = {
+  private def loadXML(params: String)(implicit st: SleepTime): Option[xml.Elem] = {
     try {
-      val source = scala.io.Source.fromURL(basedURL + params, "UTF-8")
+      val source = scala.io.Source.fromURL(AOJ_URI + params, "UTF-8")
       val str = source.mkString.replaceAll("\n", "")
       source.close()
-      Thread.sleep(SLEEP_TIME)
+      Thread.sleep(st)
 
       val ret = scala.xml.XML.loadString(str)
       Some(ret)
@@ -31,12 +37,12 @@ object AOJ extends QueryWrapper {
    *
    * @param id User ID.
    */
-  def user(id: String): Option[User] = {
+  def user(id: String)(implicit st: SleepTime = DEFAULT_SLEEP_TIME): Option[User] = {
     // check
     require(id.length >= 1, "User ID is invalid.")
 
     // get
-    val userXml = loadXML("user?id=" + id)
+    val userXml = loadXML("user?id=" + id)(st)
 
     userXml.map {
       x => User(x)
@@ -48,12 +54,12 @@ object AOJ extends QueryWrapper {
    *
    * @param id Problem ID.
    */
-  def problem(id: String): Option[Problem] = {
+  def problem(id: String)(implicit st: SleepTime = DEFAULT_SLEEP_TIME): Option[Problem] = {
     // check
     require(4 <= id.length() && id.length() <= 5 && id.forall(_.isDigit), "Problem ID is invalid.")
 
     // get
-    val problemXml = loadXML("problem?id=" + id)
+    val problemXml = loadXML("problem?id=" + id)(st)
 
     problemXml filter {
       x =>
@@ -68,12 +74,12 @@ object AOJ extends QueryWrapper {
    *
    * @param volume Volume No.
    */
-  def problem_list(volume: Int): Option[ProblemList] = {
+  def problem_list(volume: Int)(implicit st: SleepTime = DEFAULT_SLEEP_TIME): Option[ProblemList] = {
     // check
     require(0 <= volume && volume <= 100, "Volume No is invalid.")
 
     // get
-    val problem_listXml = loadXML("problem_list?volume=" + volume)
+    val problem_listXml = loadXML("problem_list?volume=" + volume)(st)
 
     problem_listXml.map {
       x => ProblemList(x)
@@ -88,7 +94,7 @@ object AOJ extends QueryWrapper {
    * @param solved_min The service returns a list of users who solved at least solved_min problems.
    * @param solved_max The service returns a list of users who solved at most solved_max problems.
    */
-  def user_list(criteria: Criteria = null, affiliation: Affiliation = null, solved_min: SolvedMin = null, solved_max: SolvedMax = null): Option[UserList] = {
+  def user_list(criteria: Criteria = null, affiliation: Affiliation = null, solved_min: SolvedMin = null, solved_max: SolvedMax = null)(implicit st: SleepTime = DEFAULT_SLEEP_TIME): Option[UserList] = {
     // check
     if (criteria != null) {
       require(criteria.value == 0 || criteria.value == 1)
@@ -98,7 +104,7 @@ object AOJ extends QueryWrapper {
     val encoded_affiliation = if (affiliation == null) null else Affiliation(URLEncoder.encode(affiliation.value, "UTF-8"))
     val query = joinQuery(criteria, encoded_affiliation, solved_min, solved_max)
 
-    val user_listXml = loadXML("user_list" + query)
+    val user_listXml = loadXML("user_list" + query)(st)
 
     user_listXml.map {
       x => UserList(x)
@@ -114,7 +120,7 @@ object AOJ extends QueryWrapper {
    * @param date_begin The service returns records after date_begin.
    * @param date_end The service returns records before date_end.
    */
-  def solved_record(user_id: UserID = null, problem_id: ProblemID = null, language: Language = null, date_begin: DateBegin = null, date_end: DateEnd = null): Option[SolvedRecord] = {
+  def solved_record(user_id: UserID = null, problem_id: ProblemID = null, language: Language = null, date_begin: DateBegin = null, date_end: DateEnd = null)(implicit st: SleepTime = DEFAULT_SLEEP_TIME): Option[SolvedRecord] = {
     // check
     require(user_id != null || problem_id != null, "User ID or Problem ID should be specified.")
     if (problem_id != null) {
@@ -124,7 +130,7 @@ object AOJ extends QueryWrapper {
     // get
     val query = joinQuery(user_id, problem_id, language, date_begin, date_end)
 
-    val solved_recordXml = loadXML("solved_record" + query)
+    val solved_recordXml = loadXML("solved_record" + query)(st)
 
     solved_recordXml.map {
       x => SolvedRecord(x)
@@ -139,7 +145,7 @@ object AOJ extends QueryWrapper {
    * @param start Start position.
    * @param limit The number of records. (1 <= limit <= 20)
    */
-  def status_log(user_id: UserID = null, problem_id: ProblemID = null, start: Start = null, limit: Limit = null): Option[StatusLog] = {
+  def status_log(user_id: UserID = null, problem_id: ProblemID = null, start: Start = null, limit: Limit = null)(implicit st: SleepTime = DEFAULT_SLEEP_TIME): Option[StatusLog] = {
     // check
     if (problem_id != null) {
       require(problem_id.value.forall(_.isDigit), "Problem ID is invalid.")
@@ -148,7 +154,7 @@ object AOJ extends QueryWrapper {
     // get
     val query = joinQuery(user_id, problem_id, start, limit)
 
-    val status_logXml = loadXML("status_log" + query)
+    val status_logXml = loadXML("status_log" + query)(st)
 
     status_logXml.map {
       x => StatusLog(x)
@@ -163,7 +169,7 @@ object AOJ extends QueryWrapper {
    * @param id Problem ID.
    * @param category Category Name.
    */
-  def problem_category(id: ID = null, category: Category = null): Option[ProblemCategory] = {
+  def problem_category(id: ID = null, category: Category = null)(implicit st: SleepTime = DEFAULT_SLEEP_TIME): Option[ProblemCategory] = {
     // check
     if (id != null) {
       require(id.value.forall(_.isDigit), "Problem ID is invalid.")
@@ -172,7 +178,7 @@ object AOJ extends QueryWrapper {
     // get
     val query = joinQuery(id, category)
 
-    val problem_categoryXml = loadXML("problem_category" + query)
+    val problem_categoryXml = loadXML("problem_category" + query)(st)
 
     problem_categoryXml.map {
       x => ProblemCategory(x)
@@ -184,12 +190,12 @@ object AOJ extends QueryWrapper {
    *
    * @param id Problem ID.
    */
-  def source(id: String): Option[Source] = {
+  def source(id: String)(implicit st: SleepTime = DEFAULT_SLEEP_TIME): Option[Source] = {
     // check
     require(id.forall(_.isDigit), "Problem ID is invalid.")
 
     // get
-    val sourceXml = loadXML("source?id=" + id)
+    val sourceXml = loadXML("source?id=" + id)(st)
 
     sourceXml.map {
       x => Source(x)
